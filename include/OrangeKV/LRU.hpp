5ed05b80a8a43fe6a2698313c1d258ff175eef1f
zhangxuan9812 <38398278+zhangxuan9812@users.noreply.github.com>
2024-03-27 11:15:48 -0700

    LRU

--- include/OrangeKV/LRU.hpp
+++ include/OrangeKV/LRU.hpp
@@ -31,8 +31,8 @@ class LRUCache {
 private:
     size_t capacity_; // The maximum capacity of the cache
     size_t usage_; // The total charge of the cache
-    std::list<LRUNode<KeyType, ValueType>> lruList; // The list of nodes that are not in use
-    std::list<LRUNode<KeyType, ValueType>> inUseList; // The list of nodes that are in use
+    std::list<LRUNode<KeyType, ValueType>*> lruList; // The list of nodes that are not in use
+    std::list<LRUNode<KeyType, ValueType>*> inUseList; // The list of nodes that are in use
     std::unordered_map<KeyType, typename std::list<LRUNode<KeyType, ValueType>*>::iterator, OrangeKV::HashBKDR> lruMap; // The map of keys to nodes
     LockType locker; // The locker for thread safety
 public:
@@ -43,6 +43,9 @@ public:
     void release(Handle* handle); // Release a node from the cache
     void erase(const KeyType& key, uint32_t hash); // Erase a node from the cache
     void prune(); // Prune the cache
+    void setCapacity(size_t capacity) { // Set the maximum capacity of the cache
+        capacity_ = capacity;
+    }
     size_t capacity() const { // Get the maximum capacity of the cache
         return capacity_;
     }
@@ -65,11 +68,12 @@ LRUCache<KeyType, ValueType, LockType>::LRUCache() : capacity_(0), usage_(0) {}
 template<typename KeyType, typename ValueType, typename LockType>
 LRUCache<KeyType, ValueType, LockType>::~LRUCache() {
     // Release all handles
+    static_assert(inUseList.empty(), "inUseList.empty()");  // The in-use list must be empty
     for (auto it = lruList.begin(); it != lruList.end(); ++it) {
-        if (it->refs > 0) {
-            // Call the deleter function
-            it->deleter(*(it->key), it->value);
-        }
+        LRUNode<KeyType, ValueType>* node = *it;
+        node->inCache = false;
+        static_assert(node->refs == 1, "node->refs == 1"); // The reference count of the node must be 1
+        unref(node);
     }
 }
 
@@ -88,32 +92,31 @@ Handle* LRUCache<KeyType, ValueType, LockType>::insert(const KeyType& key, uint3
     newNode->refs = 1;
     std::memcpy(newNode->keyData, key.data(), key.size()); // Copy the key data to the node
     // Check if the key already exists in the cache
-    auto it = lruMap.find(newNode->key);
+    //auto it = lruMap.find(newNode->key);
+    auto it  = lruMap.find(*(newNode->key));
     if (it != lruMap.end()) {
         // Key already exists, update the value and move the node to the front of the LRU list
-        LRUNode<KeyType, ValueType>* node = it->second; 
+        LRUNode<KeyType, ValueType>* node = *(it->second);  // Get the node
         newNode->refs++; // Increase the reference count of the new node
 
         lruAppend(inUseList, newNode); // Append the new node to the in-use list
-        lruMap[newNode->key] = inUseList.begin(); // Update the key in the LRU map
-        finshErase(node); // Finish erasing the old node
+        lruMap[*(newNode->key)] = inUseList.begin(); // Update the key in the LRU map
+        finishErase(node); // Finish erasing the old node
         usage_ += charge;
     }
     else {
         // Key does not exist, add the node to the in-use list and the LRU map
         newNode->refs++; // Increase the reference count of the new node
         lruAppend(inUseList, newNode); // Append the node to the in-use list
-        lruMap.emplace(newNode->key, inUseList.begin()); // Add the key to the LRU map
+        lruMap.emplace(*(newNode->key), inUseList.begin()); // Add the key to the LRU map
         usage_ += charge; // Update the cache usage
     }
     // Prune the cache if the usage exceeds the capacity
     while (usage_ > capacity_ && !lruList.empty()) {
-        LRUNode<KeyType, ValueType>* node = &lruList.back();
-        lruRemove(lruList, node); // Remove the node from the LRU list
-        lruMap.erase(node->key); // Remove the key from the LRU map
-        usage_ -= node->charge; // Update the cache usage
-        node->deleter(*(node->key), node->value); // Call the deleter function
-        delete[] reinterpret_cast<char*>(node);  // Delete the node
+        LRUNode<KeyType, ValueType>* node = lruList.back(); // Get the last node in the LRU list
+        static_assert(node->refs == 1, "node->refs == 1"); // The reference count of the node must be 1
+        lruMap.erase(*(node->key)); // Erase the key from the LRU map
+        finishErase(node); // Finish erasing the node
     }
     return reinterpret_cast<Handle*>(newNode); // Return the handle
 }
@@ -126,7 +129,7 @@ Handle* LRUCache<KeyType, ValueType, LockType>::lookUp(const KeyType& key, uint3
     // Check if the key exists in the cache
     auto it = lruMap.find(key);
     if (it != lruMap.end()) {
-        LRUNode<KeyType, ValueType>* node = it->second; // Get the node
+        LRUNode<KeyType, ValueType>* node = *(it->second); // Get the node
         if (node->hash == hash) {
             ref(node); // Increase the reference count of the node
             return reinterpret_cast<Handle*>(node);
@@ -153,18 +156,10 @@ void LRUCache<KeyType, ValueType, LockType>::erase(const KeyType& key, uint32_t 
     // Check if the key exists in the cache
     auto it = lruMap.find(key);
     if (it != lruMap.end()) {
-        LRUNode<KeyType, ValueType>* node = it->second;
+        LRUNode<KeyType, ValueType>* node = *(it->second);
         if (node->hash == hash) {
             lruMap.erase(it);
             finishErase(node);
-            // // Call the deleter function
-            // node->deleter(*(node->key), node->value);
-            
-            // Update the cache usage
-            //usage_ -= node->charge;
-            
-            // Delete the node
-            //delete[] reinterpret_cast<char*>(node);
         }
     }
 }
@@ -172,13 +167,9 @@ void LRUCache<KeyType, ValueType, LockType>::erase(const KeyType& key, uint32_t 
 template<typename KeyType, typename ValueType, typename LockType>
 void LRUCache<KeyType, ValueType, LockType>::prune() {
     while (!lruList.empty()) {
-        LRUNode<KeyType, ValueType>* node = &lruList.back();
-        lruRemove(lruList, node);
+        LRUNode<KeyType, ValueType>* node = lruList.back();
         lruMap.erase(*(node->key));
         finishErase(node);
-        // usage_ -= node->charge;
-        // node->deleter(*(node->key), node->value);
-        // delete[] reinterpret_cast<char*>(node);
     }
 }
 
@@ -204,13 +195,13 @@ void LRUCache<KeyType, ValueType, LockType>::ref(LRUNode<KeyType, ValueType>* no
 
 
 template<typename KeyType, typename ValueType, typename LockType>
-void LRUCache<KeyType, ValueType, LockType>::unref(LRUNode<KeyType, ValueType>* node) {
+void LRUCache<KeyType, ValueType, LockType>::unref(LRUNode<KeyType, ValueType>* node) { 
     node->refs--;
-    if (node->refs == 0 || node->inCache == false) {
+    if (node->refs == 0) { // Erase the node if the reference count is 0
         (*node->deleter)(*(node->key), node->value);
         free(node);
     }
-    else if (node->refs == 1 && node->inCache == true) {
+    else if (node->refs == 1 && node->inCache == true) { 
         lruRemove(inUseList, node);
         lruAppend(lruList, node);
     }
@@ -219,10 +210,11 @@ void LRUCache<KeyType, ValueType, LockType>::unref(LRUNode<KeyType, ValueType>* 
 template<typename KeyType, typename ValueType, typename LockType>
 bool LRUCache<KeyType, ValueType, LockType>::finishErase(LRUNode<KeyType, ValueType>* node) {
     if (node != nullptr) {
+        static_assert(node->inCache == true, "node->inCache == true");
         if (node->refs == 1) {
             lruRemove(lruList, node);
         }
-        else if (node->refs >= 2) {
+        else if (node->refs >= 2) { //Can't move the node to the lruList, because node->inCache will be set to false
             lruRemove(inUseList, node);
         }
         node->inCache = false;
